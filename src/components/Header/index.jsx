45782875cf3c6828f172cc7cec5c6fbb8d85e777
Lucas Leme Cardoso Ferreira <lucasleme09@gmail.com>
2024-10-30 14:01:42 -0300

    Alterado botao de logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,40 +1,16 @@
 "use client"
 
 import { signOut } from 'next-auth/react';
-import Image from 'next/image';
 import { MdLogout } from "react-icons/md";
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "../ui/popover";
 
 export const Header = () => {
     return (
         <header className="flex w-full h-[75px] py-4 px-8 justify-center items-center gap-4 bg-[#337B5B]">
             <div className="flex w-full justify-end items-center gap-3">
-                <p className="text-white">
-                    Administrador
-                </p>
-                <Popover>
-                    <PopoverTrigger>
-                        <div>
-                            <Image
-                                src="/user.png"
-                                width={40}
-                                height={40}
-                                alt="Icone de usuário"
-                                className="cursor-pointer"
-                            />
-                        </div>
-                    </PopoverTrigger>
-                    <PopoverContent>
-                        <button onClick={() => signOut({ callbackUrl: '/login', redirect:true })} className="flex flex-row">
-                            <MdLogout className="w-6 h-6 mr-2 text-[#337B5B]"/>
-                            <p className="text-lg text-[#337B5B]">Sair</p>
-                        </button>
-                    </PopoverContent>
-                </Popover>
+                <button onClick={() => signOut({ callbackUrl: '/login', redirect:true })} className="flex flex-row">
+                    <MdLogout className="w-6 h-6 mr-2 text-white"/>
+                    <p className="text-lg text-white">Sair</p>
+                </button>
             </div>
         </header>
     )
