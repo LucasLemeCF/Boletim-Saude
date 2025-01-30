6d914cf2057eac0801a468a77997cdeaf1fa5630
Lucas Leme Cardoso Ferreira <lucasleme09@gmail.com>
2025-01-30 16:00:09 -0300

    Aumentado tempo de expiracao do token JWT

--- backend/src/main/java/boletimdasaude/infra/security/TokenService.java
+++ backend/src/main/java/boletimdasaude/infra/security/TokenService.java
@@ -44,7 +44,7 @@ public class TokenService {
     }
 
     private Instant dataExpiracao() {
-        return LocalDateTime.now().plusHours(6).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(12).toInstant(ZoneOffset.of("-03:00"));
     }
 
 }
