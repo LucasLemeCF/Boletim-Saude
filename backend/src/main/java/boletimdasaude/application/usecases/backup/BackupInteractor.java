40e20f68903354b08681ea49627ab074552ef191
Lucas Leme Cardoso Ferreira <lucasleme09@gmail.com>
2025-02-05 16:10:24 -0300

    Adicionado endpoint para realizar backup do banco de dados

---
+++ backend/src/main/java/boletimdasaude/application/usecases/backup/BackupInteractor.java
@@ -0,0 +1,100 @@
+package boletimdasaude.application.usecases.backup;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class BackupInteractor {
+
+    @Value("${db.name}")
+    private String dbName;
+
+    @Value("${db.user}")
+    private String dbUser;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Value("${db.host}")
+    private String host;
+
+    @Value("${db.port}")
+    private String port;
+
+    public ResponseEntity<InputStreamResource> gerarBackup() throws IOException, InterruptedException {
+        Path arquivoTemporario = criarArquivoTemporario();
+        adicionarBackupAoArquivoTemporario(arquivoTemporario);
+        byte[] backupEmBytes = converterBackupEmBytes(arquivoTemporario);
+        InputStreamResource backupEmInputStreamResource = converterBackupEmInputStreamResource(backupEmBytes);
+        excluiArquivoTemporario(arquivoTemporario);
+        HttpHeaders headers = criarHeader();
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(backupEmBytes.length)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(backupEmInputStreamResource);
+    }
+
+    private InputStreamResource converterBackupEmInputStreamResource(byte[] arquivoEmBytes) {
+        ByteArrayInputStream backupEmByteArrayInputStream = converterParaByteArrayInputStream(arquivoEmBytes);
+        return converterParaInputStreamResource(backupEmByteArrayInputStream);
+    }
+
+    private Path criarArquivoTemporario() throws IOException {
+        return Files.createTempFile("backup_", ".sql");
+    }
+
+    private ProcessBuilder adicionarBackupAoArquivoTemporario(Path arquivoTemporario) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("pg_dump", "-h", host, "-p", port, "-U", dbUser, "-d", dbName, "-F", "c", "-b", "-v", "-f", arquivoTemporario.toString());
+        processBuilder.environment().put("PGPASSWORD", dbPassword);
+        processBuilder.redirectErrorStream(true);
+
+        Process process = processBuilder.start();
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            throw new RuntimeException("O processo de backup falhou com o código de saída: " + exitCode);
+        }
+
+        return processBuilder;
+    }
+    
+    private byte[] converterBackupEmBytes(Path arquivoTemporario) throws IOException {
+        return Files.readAllBytes(arquivoTemporario);
+    }
+    
+    private ByteArrayInputStream converterParaByteArrayInputStream(byte[] arquivoEmBytes) {
+        return new ByteArrayInputStream(arquivoEmBytes);
+    }
+    
+    private InputStreamResource converterParaInputStreamResource(ByteArrayInputStream byteArrayInputStream) {
+        return new InputStreamResource(byteArrayInputStream);
+    }
+
+    private HttpHeaders criarHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        String dataAtual = buscarDataAtual();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + dataAtual + ".sql");
+        return headers;
+    }
+
+    private String buscarDataAtual() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return LocalDate.now().format(formatter);
+    }
+
+    private void excluiArquivoTemporario(Path arquivoTemporario) throws IOException {
+        Files.delete(arquivoTemporario);
+    }
+
+}
